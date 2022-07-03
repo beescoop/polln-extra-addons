b5b2565ec4a7fda5e96759444acea31b2610a25b
Thibault Francois <francois.th@gmail.com>
2022-07-04 01:12:36 +0200

    [ADD] olln_website_product

---
+++ polln_website_product/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Polln
+#   - Thibault François
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Polln Product on website",
+    "summary": """
+        Display the available product in the shop on the website
+        No possibility to buy them
+     """,
+    "website": "https://github.com/beescoop/",
+    "category": "Sales",
+    "version": "12.0.1.0.1",
+    "depends": ["website_sale"],
+    "data": [
+        "views/templates.xml",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
